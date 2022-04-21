812ae0315867f3721c08be1c3977d445271e38bf
Martin <mgaraypagano@gmail.com>
2022-04-21 14:50:21 -0300

    update diseño header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {Container, Stack, Box, Image, Input, Icon, StackDivider, Text} from "@chakra-ui/react";
 import {AiOutlineSearch} from "react-icons/ai";
 import {FiPrinter} from "react-icons/fi";
+import {GoLocation} from "react-icons/go";
+import {IoCartOutline} from "react-icons/io5";
 
 import Logo from "../assets/logo.png";
 
@@ -11,8 +13,8 @@ const App: React.FC = () => {
   return (
     <Stack>
       <Box backgroundColor="primary.500" boxShadow="sm" paddingY={4}>
-        <Container maxW="container.xl" padding={0}>
-          <Stack>
+        <Container maxW="container.xl">
+          <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between">
               <Stack direction="row" flex={1} spacing={12}>
                 <Image objectFit="contain" src={Logo} />
@@ -38,8 +40,35 @@ const App: React.FC = () => {
                 </Text>
               </Stack>
             </Stack>
-            <Stack direction="row">
-              <Text>Envios a Buenos Aires</Text>
+            <Stack alignItems="baseline" direction="row" justifyContent="space-between">
+              <Stack alignItems="baseline" direction="row" spacing={12}>
+                <Stack alignItems="center" direction="row" spacing={1}>
+                  <Icon as={GoLocation} height={6} width={6} />
+                  <Stack spacing={0}>
+                    <Text color="blackAlpha.500" fontSize="xs" lineHeight="normal">
+                      Enviar a
+                    </Text>
+                    <Text fontSize="sm" lineHeight="normal">
+                      Buenos Aires 1639
+                    </Text>
+                  </Stack>
+                </Stack>
+                <Stack color="blackAlpha.700" direction="row" fontSize="sm" spacing={5}>
+                  <Text>Categorías</Text>
+                  <Text>Ofertas</Text>
+                  <Text>Historial</Text>
+                  <Text>Supermercado</Text>
+                  <Text>Moda</Text>
+                  <Text>Vender</Text>
+                  <Text>Ayuda</Text>
+                </Stack>
+              </Stack>
+              <Stack direction="row" fontSize="sm" lineSpacing={6}>
+                <Text>Creá tu cuenta</Text>
+                <Text>Ingresá</Text>
+                <Text>Mis compras</Text>
+                <Icon as={IoCartOutline} height={6} width={6} />
+              </Stack>
             </Stack>
           </Stack>
         </Container>
@@ -52,3 +81,15 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+{
+  /* <Stack color="blackAlpha.700" direction="row" fontSize="sm" spacing={5}>
+                    <Text>Categorías</Text>
+                    <Text>Ofertas</Text>
+                    <Text>Historial</Text>
+                    <Text>Supermercado</Text>
+                    <Text>Moda</Text>
+                    <Text>Vender</Text>
+                    <Text>Ayuda</Text>
+                  </Stack> */
+}
