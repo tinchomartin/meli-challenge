96f034cccdf4d047320862b551d1c333d982f624
Martin <mgaraypagano@gmail.com>
2022-06-22 20:51:51 -0300

    componente Qa

--- src/components/Qa.tsx
+++ src/components/Qa.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Stack, Text} from "@chakra-ui/react";
+import {Stack, Text, UnorderedList, ListItem} from "@chakra-ui/react";
 
 export const Qa = () => {
   return (
@@ -11,6 +11,81 @@ export const Qa = () => {
       <Text as="h3" fontSize="lg" fontWeight="semibold">
         ¿Qué querés saber?
       </Text>
+
+      <Stack direction="row">
+        <UnorderedList
+          color="secondary.500"
+          display="flex"
+          flexWrap="wrap"
+          fontSize="sm"
+          fontWeight="semibold"
+          listStyleType="none"
+          m="0"
+          textAlign="center"
+          whiteSpace="nowrap"
+        >
+          <ListItem
+            _hover={{background: "rgba(65,137,230,.2)"}}
+            backgroundColor="rgba(65,137,230,.15)"
+            borderRadius="5px"
+            cursor="pointer"
+            lineHeight="32px"
+            marginRight="12px"
+            marginTop="12px"
+            padding="0 12px"
+          >
+            Costo y tiempo de envío
+          </ListItem>
+          <ListItem
+            _hover={{background: "rgba(65,137,230,.2)"}}
+            backgroundColor="rgba(65,137,230,.15)"
+            borderRadius="5px"
+            cursor="pointer"
+            lineHeight="32px"
+            marginRight="12px"
+            marginTop="12px"
+            padding="0 12px"
+          >
+            Devoluciones gratis
+          </ListItem>
+          <ListItem
+            _hover={{background: "rgba(65,137,230,.2)"}}
+            backgroundColor="rgba(65,137,230,.15)"
+            borderRadius="5px"
+            cursor="pointer"
+            lineHeight="32px"
+            marginRight="12px"
+            marginTop="12px"
+            padding="0 12px"
+          >
+            Medios de pago y promociones
+          </ListItem>
+          <ListItem
+            _hover={{background: "rgba(65,137,230,.2)"}}
+            backgroundColor="rgba(65,137,230,.15)"
+            borderRadius="5px"
+            cursor="pointer"
+            lineHeight="32px"
+            marginRight="12px"
+            marginTop="12px"
+            padding="0 12px"
+          >
+            Garantía
+          </ListItem>
+          <ListItem
+            _hover={{background: "rgba(65,137,230,.2)"}}
+            backgroundColor="rgba(65,137,230,.15)"
+            borderRadius="5px"
+            cursor="pointer"
+            lineHeight="32px"
+            marginRight="12px"
+            marginTop="12px"
+            padding="0 12px"
+          >
+            Cuotas sin tarjeta
+          </ListItem>
+        </UnorderedList>
+      </Stack>
     </Stack>
   );
 };
