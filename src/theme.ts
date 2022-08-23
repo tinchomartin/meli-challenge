9e4eb27a2c573e2c7228ffe8a1cf6d49e590b62d
Martin <mgaraypagano@gmail.com>
2022-08-23 13:55:49 -0300

    nueva font

--- src/theme.ts
+++ src/theme.ts
@@ -36,8 +36,8 @@ export default extendTheme({
     },
   },
   fonts: {
-    body: "Proxima Nova",
-    heading: "Proxima Nova",
+    body:  "Proxima Nova, -apple-system,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif",
+    heading:  "Proxima Nova, -apple-system,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif",
   },
   components: {
     Link: {
